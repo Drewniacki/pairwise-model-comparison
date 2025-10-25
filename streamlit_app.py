1231f2271a6599f9a1a66e8ae4e18c1f8eef9d65
Damian Gruszecki <68701630+Drewniacki@users.noreply.github.com>
2025-10-26 04:55:22 +0800

    Refactor Streamlit app layout with tabs

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,20 @@
 import streamlit as st
 
-st.title("Terrafusion25: grading model outputs")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+# title
+st.title("Terrafusion25\ngrading model outputs")
+st.subheader(a.k.a. Geologists' input")
+st.divider()
+
+# tabs
+
+tab1, tab2 = st.tabs(["Hello", "Chunks"])
+
+with tab1:
+    st.write(
+        "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    )
+
+with tab2:
+    st.write(
+        "Grading chunking process here."
+    )
