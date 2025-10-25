8fab596997765ecd6b376288c7e7f88e1ba2eaed
Damian Gruszecki <68701630+Drewniacki@users.noreply.github.com>
2025-10-26 05:03:05 +0800

    Fix title formatting and update content in streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # title
-st.title("Terrafusion25 — grading model outputs")
+st.title("Terrafusion25: grading model outputs")
 st.caption("a.k.a. Geologists' input")
 
 # tabs
@@ -9,6 +9,22 @@ st.caption("a.k.a. Geologists' input")
 tab1, = st.tabs(["Chunks"])
 
 with tab1:
-    st.write(
-        "Grading chunking process here."
+    st.markdown(
+        "- Chunk size 
+  - right
+  - too small
+  - too big
+- Well assignment
+  - Correct
+  - no wells assigned but chunk refers to the well
+  - incorrect well assigned
+  - multiple wells assigned including correct and not correct one
+- Chunk information 
+  - processed correctly 
+  - missing information 
+  - hallucinated 
+
+Comment 
+
+General observation "
     )
