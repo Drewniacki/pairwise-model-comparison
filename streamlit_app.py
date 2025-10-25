df60745ea911ec4a52db07342476a2cf16170b84
Damian Gruszecki <68701630+Drewniacki@users.noreply.github.com>
2025-10-26 04:57:47 +0800

    Refactor Streamlit app title and tabs

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,15 @@
 import streamlit as st
 
 # title
-st.title("Terrafusion25\ngrading model outputs")
-st.subheader("a.k.a. Geologists' input")
-st.divider()
+st.title("Terrafusion25")
+st.subheader("grading model outputs")
+st.caption("a.k.a. Geologists' input")
 
 # tabs
 
-tab1, tab2 = st.tabs(["Hello", "Chunks"])
+tab1 = st.tabs(["Chunks"])
 
 with tab1:
-    st.write(
-        "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-    )
-
-with tab2:
     st.write(
         "Grading chunking process here."
     )
