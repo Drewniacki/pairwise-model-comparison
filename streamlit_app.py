5301aa815733071f5b61ad1250474fe1eaa4ce0c
Damian Gruszecki <68701630+Drewniacki@users.noreply.github.com>
2025-10-26 04:46:30 +0800

    Change app title to 'Terrafusion25: grading model outputs'

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("Terrafusion25: grading model outputs")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
